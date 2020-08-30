94a215fd2d42a3d1186e21d496205eca574ab64d
Mateusz Antczak <mati98848@gmail.com>
2020-08-30 06:17:17 +0200

    Mouse cursor update
    added fancy cursor for better gameplay

---
+++ CrunchyProject/Assets/Scripts/MouseCursor.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCursor : MonoBehaviour
+{
+    void Update()
+    {
+        Cursor.visible = false;
+        Vector2 mousePos = Input.mousePosition;
+        transform.position = mousePos;
+    }
+}
